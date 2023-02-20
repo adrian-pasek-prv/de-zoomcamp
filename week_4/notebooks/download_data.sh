b93a85c1cc825069f152c94cc0e46a0ff41243c6
adrian-pasek-prv <pasek.adrian.314@gmail.com>
2023-02-20 08:49:02 +0000

    Explore Spark SQL capabilities

--- week_4/notebooks/download_data.sh
+++ week_4/notebooks/download_data.sh
@@ -9,7 +9,7 @@ URL_PREFIX="https://github.com/DataTalksClub/nyc-tlc-data/releases/download"
 for MONTH in {1..12}; do
   FMONTH=`printf "%02d" ${MONTH}`
 
-  URL="${URL_PREFIX}/${TAXI_TYPE}_tripdata_${YEAR}-${FMONTH}.csv.gz"
+  URL="${URL_PREFIX}/${TAXI_TYPE}/${TAXI_TYPE}_tripdata_${YEAR}-${FMONTH}.csv.gz"
 
   LOCAL_PREFIX="data/raw/${TAXI_TYPE}/${YEAR}/${FMONTH}"
   LOCAL_FILE="${TAXI_TYPE}_tripdata_${YEAR}_${FMONTH}.csv.gz"
